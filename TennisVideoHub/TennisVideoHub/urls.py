b154ce5c80d2900253883ade76a944b9732632cc
HaoNan Tang <thaonan@umich.edu>
2026-08-06 21:58:19 +0800

    Iteration-01 doing

---
+++ TennisVideoHub/TennisVideoHub/urls.py
@@ -0,0 +1,69 @@
+"""
+URL configuration for TennisVideoHub project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+"""
+TODO
+Assign: JLZ
+
+Task: notes这是在干啥
+
+Why?
+
+Deliverable: 写完
+"""
+urlpatterns += [
+    path('videos/', include('videos.urls')),
+]
+
+from django.views.generic import RedirectView
+
+"""
+TODO
+Assign: JLZ
+
+Task: notes这是在干啥
+
+Why?
+
+Deliverable: 写完
+"""
+urlpatterns += [
+    path('', RedirectView.as_view(url='/videos/')),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+"""
+TODO
+Assign: JLZ
+
+Task: notes这是在干啥
+
+Why?
+
+Deliverable: 写完
+"""
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ project_management/Iteration.md
@@ -0,0 +1,26 @@
+# Iteration-01 
+
+## Observations
+
+目前啥都没有，需要搭建网站地基才可以开始。
+
+## Decision
+
+跟着 P2 的教程和经验 搭建网站的地基
+
+## Goal
+
+创建一个能启动，能连接数据库，知道以后把 “上传功能” 放在哪里的网站空架子
+
+## Change & Deliverable
+
+"""
+TODO
+Assign: JLZ
+
+Task: 根据给定模板写出这一节
+
+Why? 回顾
+
+Deliverable: 写完
+"""
